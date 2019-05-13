533c60337b97c3f33040dbeec54414190a09b347
Brian Brown <brian@thekompanee.com>
2019-05-13 15:56:54 -0500

    Masonry and Stop Video JS

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,8 +1,18 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+      // Masonry
+      $('.masonry-block').matchHeight({
+        target: $('.masonry-img')
+      });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+
+      //stop video player when modal closes
+      $('body').on('hidden.bs.modal', '.modal', function () {
+        $('video').trigger('pause');
+      });
   },
 };
