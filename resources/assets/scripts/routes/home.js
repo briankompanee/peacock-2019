f8d0c7e659486943dda6e7d7372498bdcf2cd968
Brian Brown <brian@thekompanee.com>
2019-05-13 15:57:12 -0500

    Parallax JS on home page

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -1,6 +1,36 @@
 export default {
   init() {
     // JavaScript to be fired on the home page
+
+      // PARALLAX
+      $(document).ready(function(){
+         // cache the window object
+         $window = $(window);
+
+         $('section[data-type="background"]').each(function(){
+           // declare the variable to affect the defined data-type
+           var $scroll = $(this);
+
+            $(window).scroll(function() {
+              // HTML5 proves useful for helping with creating JS functions!
+              // also, negative value because we're scrolling upwards
+              var yPos = -($window.scrollTop() / $scroll.data('speed'));
+
+              // background position
+              var coords = '0 '+ yPos + 'px';
+
+              // move the background
+              $scroll.css({ backgroundPosition: coords });
+            }); // end window scroll
+         });  // end section function
+      }); // close out script
+
+      $('.carousel').carousel({
+        pause: null
+      });
+
+      /* Create HTML5 element for IE */
+      document.createElement("section");
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
